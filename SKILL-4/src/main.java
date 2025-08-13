25602e13552fd7b843d2b26bb6b3d1d28ef3f379
P Kavya Sai 2420030322 <2420030322@klh.edu.in>
2025-08-13 11:30:18 +0530

    Add files via upload
    Week-4
    1. Write a Java program for a Book class that:
    
    Has private attributes: title, author, and price.
    
    Provides setter and getter methods for each attribute.
    
    Uses the toString() method to return a string containing all book details.
    
    In the main method, create a Book object, set values using setters, and print the object directly to show that toString() is called automatically.
    
    2. Write a Java program to demonstrate the use of the final keyword without using inheritance. The program should:
    Declare a blank final variable and initialize it inside the constructor.
    Declare a final method and call it from main.
    Declare a final reference variable pointing to a mutable object, and show that you can modify the object’s content but cannot reassign the reference.

---
+++ SKILL-4/src/main.java
@@ -0,0 +1,13 @@
+
+public class main{
+	public static void main(String[] args) {
+        book book = new book();
+
+        // Set values using setters
+        book.setTitle("The Alchemist");
+        book.setAuthor("Paulo Coelho");
+        book.setPrice(399.99);
+        System.out.println(book);
+    }
+}
+
